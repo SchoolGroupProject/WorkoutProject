0234ac5cd120ca18248a7c156cbe0ae4a9d2d037
MilesNegrete <58011516+MilesNegrete@users.noreply.github.com>
2024-02-14 20:02:48 -0900

    Got the login working. I am going to start working on the Settings menu next

--- app/src/main/java/com/example/test/MainActivity.kt
+++ app/src/main/java/com/example/test/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.test
 
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.navigation.NavigationView
 import androidx.navigation.findNavController
@@ -12,6 +14,12 @@ import androidx.navigation.ui.setupWithNavController
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
 import com.example.test.databinding.ActivityMainBinding
+import com.example.test.ui.login.LoginActivity
+import android.content.Intent
+import android.content.SharedPreferences
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import io.grpc.Context
 
 class MainActivity : AppCompatActivity() {
 
@@ -54,4 +62,26 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment_content_main)
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId){
+            R.id.action_settings -> {
+//                openSettings()
+                true
+            }
+            R.id.action_login -> {
+                openLogin()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+    private fun openLogin(){
+        val intent = Intent(this,LoginActivity::class.java)
+        startActivity(intent)
+    }
+//    private fun openSettings(){
+//        val intent = Intent(this, SettingsActivity::class.java)
+//        startActivity(intent)
+//    }
 }
