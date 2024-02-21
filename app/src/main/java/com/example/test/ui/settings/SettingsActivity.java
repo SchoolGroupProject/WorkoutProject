135b4e598e512548442a1f1b68ee58645f58daed
MilesNegrete <58011516+MilesNegrete@users.noreply.github.com>
2024-02-21 11:50:58 -0900

    Made some more buttons for the setting screen. Still have not managed to get it to launch.

--- app/src/main/java/com/example/test/ui/settings/SettingsActivity.java
+++ app/src/main/java/com/example/test/ui/settings/SettingsActivity.java
@@ -1,13 +1,10 @@
 package com.example.test.ui.settings;
 
-import android.app.Activity;
 import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import com.example.test.R;
 
-public class Settings extends AppCompatActivity {
+public class SettingsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
