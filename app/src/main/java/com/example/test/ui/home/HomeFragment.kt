1daad97e553ca1161f3e60d91c8e160cc8a545f8
MilesNegrete <58011516+MilesNegrete@users.noreply.github.com>
2024-02-21 15:42:13 -0900

    Finally got the Settings menu to launch.

--- app/src/main/java/com/example/test/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/test/ui/home/HomeFragment.kt
@@ -8,9 +8,9 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.test.databinding.FragmentHomeBinding
+import com.example.test.R
 
-private val FragmentHomeBinding.textHome: TextView
-    get() {}
+
 
 class HomeFragment : Fragment() {
 
@@ -25,17 +25,7 @@ class HomeFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
-
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-
-        val textView: TextView = binding.textHome
-       homeViewModel.text.observe(viewLifecycleOwner) {
-           textView.text = it
-       }
-        return root
+        return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
     override fun onDestroyView() {
