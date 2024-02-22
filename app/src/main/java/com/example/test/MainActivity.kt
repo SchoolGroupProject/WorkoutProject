fef6bc082e78e326dad3dd87efba8ccb05274524
Jianzhao Su <JSu@student.fullsail.edu>
2024-02-21 21:19:52 -0500

    Navigation added and junk files are taken down

--- app/src/main/java/com/example/test/MainActivity.kt
+++ app/src/main/java/com/example/test/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.test
 
 import android.os.Bundle
 import android.view.Menu
+import android.view.View
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.navigation.NavigationView
 import androidx.navigation.findNavController
@@ -14,43 +15,43 @@ import androidx.appcompat.app.AppCompatActivity
 import com.example.test.databinding.ActivityMainBinding
 import com.google.common.base.FinalizableReference
 import com.google.firebase.Firebase
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
+
 
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var MainBinding: ActivityMainBinding
+    //private lateinit var drawerLayout: DrawerLayout
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        MainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(MainBinding.root)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setSupportActionBar(MainBinding.appBarMain.toolbar)
 
 
-
-        setSupportActionBar(binding.appBarMain.toolbar)
-
-        binding.appBarMain.fab.setOnClickListener {
-
-        }
-
-        val drawerLayout: DrawerLayout = binding.drawerLayout
-        val navView: NavigationView = binding.navView
+        val drawerLayout: DrawerLayout = MainBinding.drawerLayout
+        val navView: NavigationView = MainBinding.navView
         val navController = findNavController(R.id.nav_host_fragment_content_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow
-            ), drawerLayout
-        )
+
+        appBarConfiguration = AppBarConfiguration(navController.graph,drawerLayout)
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
     }
+
+//    override fun setContentView(view: View?) {
+//        drawerLayout = layoutInflater.inflate(R.layout.activity_main,null) as DrawerLayout
+//
+//
+//
+//
+//        super.setContentView(view)
+//    }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
--- app/src/main/java/com/example/test/mealPlanner/FoodActivity.kt
+++ app/src/main/java/com/example/test/mealPlanner/FoodActivity.kt
@@ -3,22 +3,34 @@ package com.example.test.mealPlanner
 import android.content.res.Resources.NotFoundException
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import androidx.viewpager2.widget.ViewPager2
+import com.example.test.MainActivity
 import com.example.test.R
+import com.example.test.databinding.ActivityFoodBinding
+import com.example.test.databinding.ActivityMainBinding
+import com.example.test.databinding.AppBarMainBinding
+import com.google.android.material.navigation.NavigationView
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 
-class FoodActivity : AppCompatActivity() {
-
+class FoodActivity : AppCompatActivity(){
 
     private lateinit var foodLayout : TabLayout
     private lateinit var foodPager : ViewPager2
-
-
+    private lateinit var foodnavi: ActivityFoodBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_food)
+        foodnavi = ActivityFoodBinding.inflate(layoutInflater)
+        setContentView(foodnavi.root)
+
 
         foodLayout = findViewById(R.id.foodtab)
         foodPager = findViewById(R.id.foodviewpager)
@@ -32,7 +44,6 @@ class FoodActivity : AppCompatActivity() {
         }.attach()
 
 
-
-
     }
+
 }
